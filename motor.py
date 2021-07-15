2d5fe692787b1991a630aa549d6b760e25912470
Wayne Ngan <wayne.ngan@gmail.com>
2021-07-15 00:56:16 -0400

    motor script takes angle as argument

--- motor.py
+++ motor.py
@@ -1,35 +1,30 @@
 import RPi.GPIO as GPIO
 import time
+import sys
 
 # between -90 and 90 degrees
 def go_to_angle(t) :
-    duty = (t + 90)/180 * 10 + 2
+    duty = (t + 90.0)/180 * 10 + 2
     pwm.ChangeDutyCycle(duty)
-
     time.sleep(0.5)
+
+if (len(sys.argv) < 2) :
+    print(sys.argv[0] + ' angle')
+    sys.exit(0)
+
+angle = float(sys.argv[1])
 
 GPIO.setmode(GPIO.BCM)
 
-pin = 17
+pin = 4
 
 GPIO.setup(pin, GPIO.OUT)
 pwm = GPIO.PWM(pin, 50)
 
 pwm.start(2.5)
 
-go_to_angle(0)
-go_to_angle(30)
-go_to_angle(60)
-go_to_angle(90)
-go_to_angle(60)
-go_to_angle(30)
-go_to_angle(0)
-go_to_angle(-30)
-go_to_angle(-60)
-go_to_angle(-90)
-# go_to_angle(-60)
-# go_to_angle(-30)
-# go_to_angle(0)
+print('Go to angle ' + str(angle))
+go_to_angle(angle)
 
 pwm.stop()
 
